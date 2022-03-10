70c98a72e77ad20de839ea9d6f0a5fbedf83f7a9
Goutham Gummadipudi <ggummadipudi@gmail.com>
2022-03-10 13:31:47 +0530

    Changes have been comitted

---
+++ LoginPage/src/main/java/LoginServlet.java
@@ -0,0 +1,57 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String n = request.getParameter("t1");
+		String pwd = request.getParameter("t2");
+		PrintWriter out = response.getWriter();
+		
+		if (n.equals("hello") && pwd.equals("world"))
+		{
+			HttpSession hs = request.getSession();
+			hs.setAttribute("uname", n);
+			response.sendRedirect("success.html");
+		}
+		else
+		{
+			out.print("User name / password is incorrect");
+			response.sendRedirect("invalid.html");
+		}
+	}
+
+}
